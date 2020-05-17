96a410b5a9ea01b49c2b77d64f27474cc69b0f14
Ember-Aiden <62077215+Ember-Aiden@users.noreply.github.com>
2020-05-17 16:46:18 -0500

    Add files via upload

---
+++ riddles2.py
@@ -0,0 +1,48 @@
+
+#!!!BOOKS ARE A PERMANENT OPTION THAT WILL NOT BE REMOVED WHEN USED!!!
+
+correct = False
+#check if book 1 was grabbed
+
+while(correct != True):
+    if(? < player.x < ? and ? < player.y < ? '''and select button is pressed'''):
+        correct = True
+    else:
+        correct = False
+correct = False
+
+
+#unlock book 2/ add book 2 to the room
+#room?.grabables.append(book 2)
+#check if book was grabbed
+
+while(correct != True):
+    if(? < player.x < ? and ? < player.y < ? '''and select button is pressed'''):
+        correct = True
+    else:
+        correct = False
+correct = False
+
+
+#unlock book 3/ add book 3 to the room
+#room?.grabables.append(book 3)
+#check if book was grabbed
+
+while(correct != True):
+    if(? < player.x < ? and ? < player.y < ? '''and select button is pressed'''):
+        correct = True
+    else:
+        correct = False
+correct = False
+
+
+#unlock book / add book 4 to the room
+#room?.grabables.append(book 4)
+#check if book was grabbed
+
+while(correct != True):
+    if(? < player.x < ? and ? < player.y < ? '''and select button is pressed'''):
+        correct = True
+    else:
+        correct = False
+correct = False
